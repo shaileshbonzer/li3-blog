6e58bdf88d5b9ea39b9b23e637326fbcdfcc10e1
Shailesh <shailesh.bonzer@gmail.com>
2019-01-18 13:17:54 +0530

    Added filter to encrypt password in users model
    Replaced 'lithium\util\String::hash' with '\lithium\security\Hash::calculate' as String is deprecated

--- app/models/Users.php
+++ app/models/Users.php
@@ -9,6 +9,7 @@
 namespace app\models;
 
 use lithium\data\Model;
+use lithium\security\Hash;
 
 class Users extends Model {
 
@@ -23,3 +24,22 @@ class Users extends Model {
     );
 
 }
+
+//We call the applyFilter() method OUTSIDE the class to create our new filter rules
+Users::applyFilter('save', function($self, $params, $chain){
+
+    //Temporarily store our entity object so that we can manipulate it
+    $record = $params['entity'];
+
+    //If an id doesn't exist yet, then we know we're saving for the first time. If a
+    //password is provided, we need to hash it
+    if(!$record->id && !empty($record->password)){
+        $record->password = Hash::calculate($record->password);
+    }
+
+    //Write the modified object back to $params
+    $params['entity'] = $record;
+
+    //Allow the next filter to be run
+    return $chain->next($self, $params, $chain);
+});
