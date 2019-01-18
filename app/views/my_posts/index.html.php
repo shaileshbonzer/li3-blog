4f0744b131d37a2f5e03525ad969860fe805d4e3
Shailesh <shailesh.bonzer@gmail.com>
2019-01-18 12:16:39 +0530

    My post index view updated
    Added link for add post
    Updated post listing
    Added link to view post

--- app/views/my_posts/index.html.php
+++ app/views/my_posts/index.html.php
@@ -7,9 +7,22 @@
  */
 ?>
 
-<p>Posts listing</p>
+<h1>Blog</h1>
+<!--We'll hard-code a link to the add post page -->
+<p><a href="/my_posts/add/">Add Post</a></p>
 
+<!--Loop through all the posts in the $myPosts object -->
 <?php foreach($myPosts as $post): ?>
-    <div><?=$post->title ?></div>
-    <div><?=$post->body ?></div>
+    <!--Each post should be displayed with this template... -->
+    <article>
+        <h2>
+            <!--Use the id column to build a link to this post -->
+            <a href="/my_posts/view/<?=$post->id ?>/">
+                <!--Use the title column to show the post title -->
+                <?=$post->title ?>
+            </a>
+        </h2>
+        <!--Use the body column to show the posts contents -->
+        <p><?=$post->body ?></p>
+    </article>
 <?php endforeach; ?>
