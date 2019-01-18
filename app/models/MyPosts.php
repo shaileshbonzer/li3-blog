5e029fbedcf5fe5beb161c8a79627fa67dec9430
Shailesh <shailesh.bonzer@gmail.com>
2019-01-18 11:42:50 +0530

    My posts model added

---
+++ app/models/MyPosts.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shailesh
+ * Date: 18/1/2019
+ * Time: 11:37 AM
+ */
+
+//Use the app\models namespace at the beginning of all model files
+namespace app\models;
+
+use lithium\data\Model;
+
+//All new models must extend the \lithium\data\Model class
+class MyPosts extends Model {
+
+    //The MyPosts class is intentionally empty right now
+
+}
+
+?>
